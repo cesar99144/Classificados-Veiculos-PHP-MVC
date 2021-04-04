ae7dd5229e411dbb67c2618dfde55fb3f8070b7f
cesar99144 <cesarferreiraa67@gmail.com>
2021-04-04 17:53:44 -0300

    Usuarios models

---
+++ App/Models/Usuario.php
@@ -0,0 +1,76 @@
+<?php
+
+use App\Core\Conexao;
+
+class Usuario extends Conexao{
+
+ 	private $nome, $sobrenome, $cidade, $uf, $email, $senha, $dataCadastro;
+
+ 	public function getNome(){
+
+ 		return $this->nome;
+ 	}
+ 	
+ 	public function setNome($nome){
+
+ 		$this->nome = $nome;
+ 	
+ 	}
+
+ 	public function getSobrenome(){
+
+ 		return $this->sobrenome;
+ 	}
+ 	
+ 	public function setSobrenome($sobrenome){
+
+ 		$this->sobrenome = $sobrenome;
+ 	
+ 	}
+
+ 	public function getCidade(){
+ 		return $this->cidade;
+ 	}
+ 	
+ 	public function setCidade($cidade){
+ 		$this->cidade = $cidade;
+ 	
+ 	}
+
+ 	public function getUf(){
+ 		return $this->uf;
+ 	}
+ 	
+ 	public function setUf($uf){
+ 		$this->uf = $uf;
+ 	
+ 	}
+
+ 	public function getEmail(){
+ 		return $this->email;
+ 	}
+ 	
+ 	public function setEmail($email){
+ 		$this->email = $email;
+ 	
+ 	}
+
+ 	public function getSenha(){
+ 		return $this->senha;
+ 	}
+ 	
+ 	public function setSenha($senha){
+ 		$this->senha = $senha;
+ 	
+ 	}
+
+ 	public function getDataCadastro(){
+ 		return $this->dataCadastro;
+ 	}
+ 	
+ 	public function setDataCadastro($dataCadastro){
+ 		$this->dataCadastro = $dataCadastro;
+ 	
+ 	}
+
+ }
---
+++ App/Models/UsuariosDao.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Core\Conexao;
+
+class UsuariosDao extends Conexao{
+
+	function cadastrarUsuario(Usuario $u){
+
+		$query = "INSERT INTO usuarios (nome, sobrenome, cidade, uf, email, senha, dataCadastro) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		$stmt = Conexao::getConn()->prepare($query);
+		$stmt->bindValue(1, $u->getNome());
+		$stmt->bindValue(2, $u->getSobrenome());
+		$stmt->bindValue(3, $u->getCidade());
+		$stmt->bindValue(4, $u->getUf());
+		$stmt->bindValue(5, $u->getEmail());
+		$stmt->bindValue(6, $u->getSenha());
+		$stmt->bindValue(7, $u->getDataCadastro());
+
+		if($stmt->execute()):
+
+			 echo "<script>alert('".$u->getNome()." cadastrado realizado com sucesso'); location ='/usuarios/login'; </script>";
+
+		else:
+
+			echo "<script>alert('Erro ao cadastrar. Verifique os dados e tente novamente'); location ='/usuarios/cadastro'; </script>";
+			//print_r($stmt->errorInfo());
+		endif;
+	} 
+
+
+	function buscarUsuario($id){
+
+		$query = "SELECT * FROM usuarios WHERE id = ?";
+		$stmt = Conexao::getConn()->prepare($query);
+		$stmt->bindValue(1, $id);
+		$stmt->execute();
+
+		if($stmt->rowCount() > 0):
+
+			$resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+			return $resultado;
+
+     	else:
+
+     		return [];
+
+     	endif;
+	}
+}
